13709b897306ec3ea478e1277e33f0b337faa439
smilezhaop <805673650@qq.com>
2018-04-09 15:48:00 +0800

    try

--- product.api/src/main/java/com/qingguatang/product/api/CategoryApi.java
+++ product.api/src/main/java/com/qingguatang/product/api/CategoryApi.java
@@ -2,12 +2,13 @@ package com.qingguatang.product.api;
 
 import com.qingguatang.product.model.Category;
 import com.qingguatang.product.model.Result;
+import com.qingguatang.product.service.CategoryService;
 import java.util.List;
 
 /**
  * 类目API：提供类目创建、修改、删除、排序功能
  */
-public interface CategoryApi {
+public interface CategoryApi extends CategoryService{
 
   public Result<List<Category>> save(List<Category> categorys);
 
