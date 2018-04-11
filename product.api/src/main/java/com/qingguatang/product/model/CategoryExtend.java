04d4863bed72cd3dc9c5f6965a07e9d673a26aa2
smilezhaop <805673650@qq.com>
2018-04-11 11:07:32 +0800

    try

--- product.api/src/main/java/com/qingguatang/product/model/CategoryExtend.java
+++ product.api/src/main/java/com/qingguatang/product/model/CategoryExtend.java
@@ -1,6 +1,54 @@
 package com.qingguatang.product.model;
 
+import java.util.List;
+
 public class CategoryExtend extends Category{
 
+  private String id;
 
+  private String name;
+
+  private String parentCategoryId;
+
+  private List<CategoryExtend> categoryExtends;
+
+  @Override
+  public String getId() {
+    return id;
+  }
+
+  @Override
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String getParentCategoryId() {
+    return parentCategoryId;
+  }
+
+  @Override
+  public void setParentCategoryId(String parentCategoryId) {
+    this.parentCategoryId = parentCategoryId;
+  }
+
+  @Override
+  public List<CategoryExtend> getCategoryExtends() {
+    return categoryExtends;
+  }
+
+  public void setCategoryExtends(
+      List<CategoryExtend> categoryExtends) {
+    this.categoryExtends = categoryExtends;
+  }
 }
