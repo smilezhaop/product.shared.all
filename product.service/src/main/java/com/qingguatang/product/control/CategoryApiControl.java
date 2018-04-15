83271df7e1e9253a2f8d7a3aa0a1d84c46e50c72
smilezhaop <805673650@qq.com>
2018-04-15 23:11:14 +0800

    try

--- product.service/src/main/java/com/qingguatang/product/control/CategoryApiControl.java
+++ product.service/src/main/java/com/qingguatang/product/control/CategoryApiControl.java
@@ -6,9 +6,14 @@ import com.qingguatang.product.dao.MediaDAO;
 import com.qingguatang.product.dataobject.CategoryDO;
 import com.qingguatang.product.model.Category;
 
+import com.qingguatang.product.model.CategoryExtend;
 import com.qingguatang.product.model.Media;
 import com.qingguatang.product.model.Result;
+import com.qingguatang.product.param.CategoryQueryParam;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import jdk.internal.org.objectweb.asm.tree.TryCatchBlockNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -17,7 +22,7 @@ import org.springframework.stereotype.Controller;
  * @date 2018/4/9
  */
 @Controller
-public class CategoryApiControl implements CategoryApi {
+public class CategoryApiControl extends CategorySearchApiControl implements CategoryApi {
 
   @Autowired
   private CategoryDAO categoryDAO;
@@ -32,37 +37,62 @@ public class CategoryApiControl implements CategoryApi {
   @Override
   public Result<List<Category>> save(List<Category> categorys) {
 
+    Result results = new Result<List<Category>>();
+    results.setSuccess(true);
 
-    //得到新类目数据
+    CategoryQueryParam param = new CategoryQueryParam();
+    //获取历史类目数据
+    List<Category> categoryList =this.query(param);
+
+    Map<String, Category> categoryMapOld = new LinkedHashMap<>();
+    //把List转换为Map对象
+    for (Category categoryDO : categoryList) {
+      Category category = new Category();
+      categoryMapOld.put(categoryDO.getId(),category);
+    }
+
+    //新类目数据
+    Map<String, Category> categoryMapNew = new LinkedHashMap<>();
+    //把List转换为Map对象
+    for (Category categoryNew : categorys) {
+      Category category = new Category();
+      categoryMapNew.put(categoryNew.getId(),category);
+    }
+
     for (Category category:categorys
     ) {
-      CategoryDO categoryDO = new CategoryDO();
-      categoryDO.setId(category.getId());
-      categoryDO.setName(category.getName());
-      categoryDO.setMediaId(category.getMediaId());
-      categoryDO.setStatus(category.getStatus().toString());
-      categoryDO.setSort(category.getSort());
-      categoryDO.setParentCategoryId(category.getParentCategoryId());
-      //对新类目名称非法验证
-
-
-      //查询历史数据
-      String categoryId = category.getId();
-      //遍历类目，判断是否存在
-      if (categoryDAO.selectById(categoryId)==null){
-        //不存在，添加
+      //判断类目是否存在
+      if(categoryMapOld.containsKey(category.getId())){//存在
+        //判断历史类目ID是否存在
+        for (Category categoryl:categoryList
+        ) {
+          if(categoryMapNew.containsKey(categoryl.getId())){//存在，修改更新
+            CategoryDO categoryDO = getCategoryDO(category);
+            categoryDAO.update(categoryDO);
+          }else{//不存在，删除
+            categoryDAO.delete(categoryl.getId());
+          }
+        }
+      }else{//不存在，增加insert
+        CategoryDO categoryDO = getCategoryDO(category);
         categoryDAO.insert(categoryDO);
-        //添加关联media
-
-
-      }else{
-        //存在 修改更新
-        categoryDAO.update(categoryDO);
 
       }
     }
+    results.setData(categorys);
 
-    return null;
+    return results;
+  }
+
+  private CategoryDO getCategoryDO(Category category) {
+    CategoryDO categoryDO = new CategoryDO();
+    categoryDO.setId(category.getId());
+    categoryDO.setName(category.getName());
+    categoryDO.setMediaId(category.getMediaId());
+    categoryDO.setStatus(category.getStatus().toString());
+    categoryDO.setSort(category.getSort());
+    categoryDO.setParentCategoryId(category.getParentCategoryId());
+    return categoryDO;
   }
 
 
